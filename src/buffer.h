acbb95be4ce6a545380f822055385e763f65686c
Alper Akcan <alper.akcan@gmail.com>
2026-08-15 23:39:19 +0300

    buffer: grow size to 4K, shrink size to 64K

--- src/buffer.h
+++ src/buffer.h
@@ -32,7 +32,7 @@ enum {
 #define MEDUSA_BUFFER_EVENT_DESTROY             MEDUSA_BUFFER_EVENT_DESTROY
 };
 
-#define MEDUSA_BUFFER_DEFAULT_GROW_SIZE         65536
+#define MEDUSA_BUFFER_DEFAULT_GROW_SIZE         4096
 #define MEDUSA_BUFFER_DEFAULT_SHRINK_SIZE       65536
 
 struct medusa_buffer_init_options {
